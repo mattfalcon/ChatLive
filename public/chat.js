e8a80928e6fd6b21aea967e5d09e27c10154d0ea
Matt Valladarez <mattvalladarez@gmail.com>
2017-04-23 10:01:24 -0500

    Add files via upload

---
+++ public/chat.js
@@ -0,0 +1,4 @@
+var myFirebase = new ('https://chatlive-911f8.firebaseio.com/');
+var usernameInput = document.querySelector('#username');
+var textInput = document.querySelector('#text');
+var postButton = document.quertySelector('#post');
